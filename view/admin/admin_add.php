985995995b2343365ac65b3736c59edc10eb40df
Jamy <jamyarrahman82@gmail.com>
2020-11-20 08:19:43 +0600

    add admin done

--- view/admin/admin_add.php
+++ view/admin/admin_add.php
@@ -1,3 +1,10 @@
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,7 +37,59 @@
                 </ul>                
             </td>
             <td>
-                <h1 align="center">Welcome Admin <br/>
+            <h1 align='center'>Registration</h1>
+    <center>
+        <form method="POST" action="../../php/admin/adRegistrationCheck.php">
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" name="name" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input type="text" name="email"></td>
+                    </tr>
+                    <tr>
+                        <td>User Name</td>
+                        <td><input type="text" name='user_name'></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="password" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>Confirm password</td>
+                        <td><input type="password" name="con_pas" id=""></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <fieldset>
+                                <legend>Gender</legend>
+                                <input type="radio" name="gen" id="" value="Male">Male
+                                <input type="radio" name="gen" id="" value="Female">Female
+                                <input type="radio" name="gen" id="" value="Other">Other
+                            </fieldset>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <fieldset>
+                                <legend>Date Of Birth</legend>
+                                <input type="date" name="dob" id="">
+                            </fieldset>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="submit" id="">
+                            <button type="reset">Reset</button>
+                        </td>
+
+                    </tr>
+                    
+                </table>
+        </form>
 							
 							
 				</h1>
@@ -39,3 +98,37 @@
     </table>
 </body>
 </html>
+
+<?php
+
+    if(isset($_SESSION['reg_msg']))
+    {
+        if( $_SESSION['reg_msg'] == 'User Name exists.')
+        {
+            echo '<script language="javascript">alert("User Name exists.")</script>';
+        }
+
+        elseif($_SESSION['reg_msg'] == 'Registraion Complete')
+        {
+            echo '<script language="javascript">alert("Registraion Complete.")</script>';
+        }
+
+        elseif($_SESSION['reg_msg'] == 'Fill Up the Form')
+        {
+            echo '<script language="javascript">alert("Fill Up the Form.")</script>';
+        }
+
+        elseif($_SESSION['reg_msg'] == 'Use Provided Link.')
+        {
+            echo '<script language="javascript">alert("Use Provided Link.")</script>';
+        }
+    
+        unset($_SESSION['reg_msg']);
+    }
+    
+
+?>
+
+<center>
+
+</center>
